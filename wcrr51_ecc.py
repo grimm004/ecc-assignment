9631549aad503a9645529b1aed2dcc1d00a0a37e
grimm004 <grimm004@gmail.com>
2019-02-06 14:05:41 +0000

    Finished coursework.

--- wcrr51_ecc.py
+++ wcrr51_ecc.py
@@ -49,28 +49,34 @@ def decimalToVector(n,r):
     return v
 
 
-def vectorToDecimal(v):
-    return sum(v[i] * (2 ** (len(v) - i - 1)) for i in range(len(v)))
+def printMatrix(m):
+    if len(m) == 0:
+        print("NaM")
+        return
+    mt = "["
+    for i in range(len(m)):
+        mt += "["
+        for j in range(len(m[0])):
+            mt += "%s, " % m[i][j]
+        mt = mt[:-2]
+        mt += "],\n "
+    mt = mt[:-3]
+    mt += "]"
+    print(mt)
 
 
-def vectorToMatrix(v):
-    return [v]
+vectorToDecimal = lambda v: sum(v[i] * (2 ** (len(v) - i - 1)) for i in range(len(v)))
+vectorToMatrix = lambda v: [v]
+matrixToVector = lambda m: m[0] if len(m) > 0 else []
 
+getH = lambda n: [[(j >> (n - i - 1)) & 1 for j in range(1, 2 ** n)] for i in range(n)]
+getN = lambda r: (2 ** r) - 1
+getK = lambda r: getN(r) - r
 
-def matrixToVector(m):
-    return m[0]
-
-
-def getH(n):
-    return [[(j >> (n - i - 1)) & 1 for j in range(1, 2 ** n)] for i in range(n)]
-
-
-def getN(r):
-    return (2 ** r) - 1
-
-
-def getK(r):
-    return getN(r) - r
+multiplyMatrix = lambda m0, m1: [] if len(m0) == 0 or len(m1) == 0 or (len(m0[0]) != len(m1)) else [[sum([(m0[i][k] * m1[k][j]) for k in range(len(m1))]) % 2 for j in range(len(m1[0]))] for i in range(len(m0))]
+zeroMatrix = lambda m, n: [[0 for _ in range(n)] for _ in range(m)]
+identityMatrix = lambda n: [[1 if i == j else 0 for i in range(n)] for j in range(n)]
+transposeMat = lambda m: [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
 
 
 def validateCodeword(l):
@@ -111,40 +117,25 @@ def message(a):
     return [(l_bin[i] if i < r else (a[i - r] if i < r + l else 0)) for i in range(k)]
 
 
-def multMat(m0, m1):
-    if len(m0) == 0 or len(m1) == 0 or (len(m0[0]) != len(m1)): return []
-    return [[sum([(m0[i][k] * m1[k][j]) for k in range(len(m1))]) % 2 for j in range(len(m1[0]))] for i in range(len(m0))]
-
-
-def zeroMat(m, n):
-    return [[0 for _ in range(n)] for _ in range(m)]
-
-
-def identMat(n):
-    return [[1 if i == j else 0 for i in range(n)] for j in range(n)]
-
-
-def transposeMat(m):
-    return [[m[j][i] for j in range(len(m))] for i in range(len(m[0]))]
-
-
 def hammingEncoder(m):
     valid, r, _ = validateCodeword(len(m))
-    return matrixToVector(multMat(vectorToMatrix(m), hammingGeneratorMatrix(r))) if valid else []
+    return matrixToVector(multiplyMatrix(vectorToMatrix(m), hammingGeneratorMatrix(r))) if valid else []
 
 
 def hammingDecoder(v):
+    v = v.copy()
     valid, r, _ = validateHammingcode(len(v))
     if not valid: return []
-    i = vectorToDecimal(matrixToVector(multMat(vectorToMatrix(v), transposeMat(getH(r))))) - 1
-    v[i] = (v[i] + 1) % 2
+    i = vectorToDecimal(matrixToVector(multiplyMatrix(vectorToMatrix(v), transposeMat(getH(r))))) - 1
+    if i > -1: v[i] = (v[i] + 1) % 2
     return v
 
 
 def messageFromCodeword(c):
-    valid, r, _ = validateHammingcode(len(c))
+    valid, _, n = validateHammingcode(len(c))
     if not valid: return []
-    return [c[(2 ** i) - 1] for i in range(r)]
+    m = [c[i] for i in range(n) if not (((i + 1) & i) == 0)]
+    return m
 
 
 def dataFromMessage(m):
@@ -165,41 +156,104 @@ def repetitionEncoder(m, n):
 
 def repetitionDecoder(v):
     if len(v) == 0: return []
-    zero_count = sum(1 for b in v if b == 0)
-    one_count = len(v) - zero_count
+    one_count = sum(v)
+    zero_count = len(v) - one_count
     if zero_count == one_count: return []
     return [0 if zero_count > one_count else 1]
 
 
-def printMat(m):
-    if len(m) == 0:
-        print("NaM")
-        return
-    mt = "["
-    for i in range(len(m)):
-        mt += "["
-        for j in range(len(m[0])):
-            mt += "%s, " % m[i][j]
-        mt = mt[:-2]
-        mt += "],\n "
-    mt = mt[:-3]
-    mt += "]"
-    print(mt)
-
-
 if __name__ == '__main__':
-    print(messageFromCodeword(hammingDecoder(hammingEncoder(message([0, 1, 1])))))
+    from random import randint
+    allPass = True
+    for i in range(100000):
+        numberToSend = randint(0, (2**16) - 1)
+        nBits = 16
+        originalData = decimalToVector(numberToSend, nBits)
+        encodedMessage = message(originalData)
+        hammingEncodedMessage = hammingEncoder(encodedMessage)
+
+        receivedHammingMessage = hammingEncodedMessage.copy()
+        receivedHammingMessage[randint(0, len(receivedHammingMessage) - 1)] ^= 1
+
+        hammingDecodedMessage = hammingDecoder(receivedHammingMessage)
+        decodedMessage = messageFromCodeword(hammingDecodedMessage)
+        receivedData = dataFromMessage(decodedMessage)
+        receivedNumber = vectorToDecimal(receivedData)
+
+        allPass &= numberToSend == receivedNumber
+
+    print("all passed" if allPass else "all failed")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     """
-    print(dataFromMessage(message([1])) == [1])
-    print(dataFromMessage(message([0, 0, 1])) == [0, 0, 1])
-    print(dataFromMessage(message([0, 1, 1, 0])) == [0, 1, 1, 0])
-    print(dataFromMessage(message([1, 1, 1, 1, 0, 1])) == [1, 1, 1, 1, 0, 1])
-    print(dataFromMessage(message([0, 1, 1, 0, 1])) == [0, 1, 1, 0, 1])
+    correct = True
+    correct &= dataFromMessage(message([1])) == [1]
+    correct &= dataFromMessage(message([0, 0, 1])) == [0, 0, 1]
+    correct &= dataFromMessage(message([0, 1, 1, 0])) == [0, 1, 1, 0]
+    correct &= dataFromMessage(message([1, 1, 1, 1, 0, 1])) == [1, 1, 1, 1, 0, 1]
+    correct &= dataFromMessage(message([0, 1, 1, 0, 1])) == [0, 1, 1, 0, 1]
+    print("message() and dataFromMessage():", "pass" if correct else "fail")
+    
+    correct = True
+    correct &= hammingEncoder([1, 1, 1]) == []
+    correct &= hammingEncoder([1, 0, 0, 0]) == [1, 1, 1, 0, 0, 0, 0]
+    correct &= hammingDecoder([1, 0, 1, 1]) == []
+    correct &= hammingDecoder([0, 1, 1, 0, 0, 0, 0]) == [1, 1, 1, 0, 0, 0, 0]
+    print("hammingEncoder() and hammingDecoder():", "pass" if correct else "fail")
+    
+    correct = True
+    correct &= messageFromCodeword([1, 1, 0, 1]) == []
+    correct &= messageFromCodeword([1, 1, 1, 0, 0, 0, 0]) == [1, 0, 0, 0]
+    print("messageFromCodeword():", "pass" if correct else "fail")
+    
+    correct = True
+    correct &= repetitionEncoder([0], 4) == [0, 0, 0, 0]
+    correct &= repetitionDecoder([1, 0, 0, 0]) == [0]
+    print("repetitionEncoder() and repetitionDecoder():", "pass" if correct else "fail")
+    
     for i in range(16):
-        a = (i >> 3) & 1
-        b = (i >> 2) & 1
-        c = (i >> 1) & 1
-        d = (i >> 0) & 1
+        a = (i >> 0) & 1
+        b = (i >> 1) & 1
+        c = (i >> 2) & 1
+        d = (i >> 3) & 1
         v = [a, b, c, d]
         encoded = hammingEncoder(v)
         hammingDecoder(encoded)
